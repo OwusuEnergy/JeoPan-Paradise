78032945cfbb188f1a73f3d2806ef27e1f7647f0
Christian Owusu <owusubusiness1@gmail.com>
2025-11-24 09:44:15 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/checkout/checkout-form.tsx
+++ src/components/checkout/checkout-form.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
@@ -538,5 +539,3 @@ export default function CheckoutForm() {
     </div>
   );
 }
-
-    
